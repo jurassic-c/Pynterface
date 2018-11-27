a4be5fec760f0d4d33938d1042b39c153785be92
Colby Sollars <colby@nucleartuxedo.com>
2018-11-27 11:09:56 -0700

    Scrollable scrollbars now only rebuild their handle image if the child elements change

--- Pynterface/scrollable.py
+++ Pynterface/scrollable.py
@@ -35,6 +35,9 @@ class scrollable(container):
 	bg_image_nineslice = ""
 	scrollbar_width = 15
 	last_pos = None
+	handles_dirty = False
+	vert_handle_surf = None
+	horiz_handle_surf = None
 	def __init__(self, w, h):
 		container.__init__(self)
 		self.width = w
@@ -74,6 +77,7 @@ class scrollable(container):
 		self.child_elements[0].move(0-self.x_offset, 0-self.y_offset)
 
 	def add(self, elem):
+		self.handles_dirty = True
 		if len(self.child_elements) > 0:
 			raise ValueError("Scrollable can only contain one child element")
 		container.add(self, elem)
@@ -121,6 +125,7 @@ class scrollable(container):
 			self.scrollbar_width = self.options["scrollbar_width"]
 		self.draw_horiz_scrollbar()
 		self.draw_vert_scrollbar()
+		self.handles_dirty = False
 		self.target_surface.blit(self.surf, self.rect)
 	
 	def get_focus_grid(self):
@@ -135,22 +140,24 @@ class scrollable(container):
 			handle_h = (self.rect.h**2 / self.child_elements[0].rect.h)
 			if handle_h < self.scrollbar_width:
 				handle_h = self.scrollbar_width
-			handle_surf = pygame.Surface((self.scrollbar_width, handle_h), pygame.locals.SRCALPHA)
 			handle_y_offset = 2
 			handle_x_offset = 2
-			if self.fg_image:
-				if self.fg_image_nineslice:
-					self.fg_image_nineslice.apply_to_surface(handle_surf)
+			if self.handles_dirty:
+				handle_surf = pygame.Surface((self.scrollbar_width, handle_h), pygame.locals.SRCALPHA)
+				if self.fg_image:
+					if self.fg_image_nineslice:
+						self.fg_image_nineslice.apply_to_surface(handle_surf)
+					else:
+						handle_h = self.fg_image.get_rect().h
+						handle_surf = self.fg_image
 				else:
-					handle_h = self.fg_image.get_rect().h
-					handle_surf = self.fg_image
-			else:
-				handle_surf.fill(self.fg_color, (2,2,self.scrollbar_width-4,handle_h-4))
+					handle_surf.fill(self.fg_color, (2,2,self.scrollbar_width-4,handle_h-4))
+				self.vert_handle_surf = handle_surf
 			y_offset = 0
 			if self.max_y_offset > 0:
 				y_offset = (float(self.y_offset)/float(self.max_y_offset))*(self.rect.h-handle_h-self.scrollbar_width*self.show_horiz_multiplier)
 				
-			self.surf.blit(handle_surf, (self.rect.w-self.scrollbar_width, y_offset))
+			self.surf.blit(self.vert_handle_surf, (self.rect.w-self.scrollbar_width, y_offset))
 
 	def draw_horiz_scrollbar(self):
 		if self.child_elements[0].rect.w > self.rect.w:
@@ -159,19 +166,22 @@ class scrollable(container):
 			self.surf.blit(scrollbar_surf, (0, self.rect.h-self.scrollbar_width))
 
 			handle_w = (self.rect.w**2 / self.child_elements[0].rect.w)
-			if handle_w < self.scrollbar_width:
-				handle_w = self.scrollbar_width
-			handle_surf = pygame.Surface((handle_w, self.scrollbar_width), pygame.locals.SRCALPHA)
 			handle_y_offset = 2
 			handle_x_offset = 2
-			if self.fg_image:
-				if self.fg_image_nineslice:
-					self.fg_image_nineslice.apply_to_surface(handle_surf)
+			if handle_w < self.scrollbar_width:
+				handle_w = self.scrollbar_width
+			if self.handles_dirty:
+				handle_surf = pygame.Surface((handle_w, self.scrollbar_width), pygame.locals.SRCALPHA)
+				if self.fg_image:
+					if self.fg_image_nineslice:
+						self.fg_image_nineslice.apply_to_surface(handle_surf)
+					else:
+						handle_w = self.fg_image.get_rect().w
+						handle_surf = self.fg_image
 				else:
-					handle_w = self.fg_image.get_rect().w
-					handle_surf = self.fg_image
-			else:
-				handle_surf.fill(self.fg_color, (2, 2, handle_w-4, self.scrollbar_width-4))
+					handle_surf.fill(self.fg_color, (2, 2, handle_w-4, self.scrollbar_width-4))
+				self.horiz_handle_surf = handle_surf
+
 			x_offset = 0
 			if self.max_x_offset > 0:
 				x_offset = (float(self.x_offset)/float(self.max_x_offset))*(self.rect.w-handle_w-self.scrollbar_width*self.show_vert_multiplier)
